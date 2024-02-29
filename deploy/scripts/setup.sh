5e145885c7f1eee37b2c3154c2b995ef416ce25b
Aryaroop Majumder <136429863+aryaroopmajumder@users.noreply.github.com>
2024-02-29 13:12:25 +0530

    fixed the container name :smile:

--- deploy/scripts/setup.sh
+++ deploy/scripts/setup.sh
@@ -9,9 +9,6 @@ fi
 # Set non-interactive frontend to avoid prompts from apt-get
 export DEBIAN_FRONTEND=noninteractive
 
-# Update system and install necessary packages
-apt-get update && apt-get install -y curl python3-pip
-
 # Attempt to install ollama if it's not already installed
 if ! command -v ollama >/dev/null 2>&1; then
     echo "ollama is not installed. Installing now."
@@ -26,13 +23,5 @@ if command -v ollama >/dev/null 2>&1; then
     ollama pull llama2
 else
     echo "Installation failed. ollama could not be installed."
-    exit 1
-fi
-
-# Install Python packages
-if [ -f "./requirements.txt" ]; then
-    pip install -r ./requirements.txt
-else
-    echo "requirements.txt file does not exist in the specified path."
     exit 1
 fi
